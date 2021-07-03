5efe7deee5c0fa3e7b40c2fcb30c514410dd491a
Cristhian Velazco <cristhianvs@yahoo.com>
2021-07-02 23:09:38 -0500

    Update BlockchainController.js
    I have added the endpoint to call the validateChain() function

--- BlockchainController.js
+++ BlockchainController.js
@@ -17,6 +17,7 @@ class BlockchainController {
         this.submitStar();
         this.getBlockByHash();
         this.getStarsByOwner();
+        this.validateChain();
     }
 
     // Enpoint to Get a Block by Height (GET Endpoint)
@@ -115,6 +116,18 @@ class BlockchainController {
                 return res.status(500).send("Block Not Found! Review the Parameters!");
             }
             
+        });
+    }
+
+
+    validateChain() {
+        this.app.get("/validateChain", async (req, res) =>{
+            let validationLog = await this.blockchain.validateChain();
+            if(validationLog.lenght==0){
+                return res.status(200).send("Blockchain looks good!");
+            } else {
+                return res.status(404).send("Blockchain has errors!");
+            }
         });
     }
 
